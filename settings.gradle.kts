24edc6721927c99d0eaed9d866449471b061879b
jeonghamin <jhamin0511@gmail.com>
2023-10-18 14:48:47 +0900

    feat(Core): design 모듈 생성

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,7 @@ dependencyResolutionManagement {
 
 rootProject.name = "Android"
 include(":app")
+include(":core:design")
 include(":ModelViewController:mvc")
 include(":ModelViewController:model")
 include(":ModelViewController:view")
