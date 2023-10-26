7be4282fc26395153f8b4891ec8b4c6283c91f62
jeonghamin <jhamin0511@gmail.com>
2023-10-26 10:12:43 +0900

    feat(Thread): ConsumerProducer Pattern with synchronized

---
+++ Asynchronous/Thread/src/main/java/com/github/jhamin0511/android/async/thread/ProdConsSync.java
@@ -0,0 +1,41 @@
+package com.github.jhamin0511.android.async.thread;
+
+import java.util.LinkedList;
+
+public class ProdConsSync {
+    private final LinkedList<Integer> list = new LinkedList<>();
+    private static final int LIMIT = 10;
+    private final Object lock = new Object();
+
+    // Synchronized로 작업 생산 동기화
+    public void produce() throws InterruptedException {
+        int value = 0;
+
+        while (true) {
+            synchronized (lock) {
+                while (list.size() == LIMIT) {
+                    lock.wait();
+                }
+                Thread.sleep(500);
+                System.out.println("[" + Thread.currentThread().getName() + "] " + "Produce : " + value);
+                list.add(value++);
+                lock.notify();
+            }
+        }
+    }
+
+    // Synchronized로 작업 소비 동기화
+    public void consume() throws InterruptedException {
+        while (true) {
+            synchronized (lock) {
+                while (list.size() == 0) {
+                    lock.wait();
+                }
+                Thread.sleep(1000);
+                int value = list.removeFirst();
+                System.out.println("[" + Thread.currentThread().getName() + "] " + "Consume : " + value);
+                lock.notify();
+            }
+        }
+    }
+}
